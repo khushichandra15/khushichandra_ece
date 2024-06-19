4b84aceb9c54f4f7cbd51c86562b79b8d32a352b
khushichandra15 <146455388+khushichandra15@users.noreply.github.com>
2024-06-19 19:02:36 +0530

    ques9.py

--- ques9.py
+++ ques9.py
@@ -1,16 +1,6 @@
-nterms = int(input("How many terms : "))
-n1, n2 = 0, 1
-count = 0
-if nterms <= 0:
-   print("Please enter a positive integer")
-elif nterms == 1:
-   print("Fibonacci sequence upto",nterms,":")
-   print(n1)
+string=input("enter string : ")
+substring=input("enter substring you want to search : ")
+if substring in string:
+    print("Substring found!")
 else:
-   print("Fibonacci sequence:")
-   while count < nterms:
-       print(n1)
-       nth = n1 + n2
-       n1 = n2
-       n2 = nth
-       count += 1
+    print("Substring not found.")
